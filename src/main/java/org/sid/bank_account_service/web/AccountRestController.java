e483c312c6056cd3e77f6a8e92da739264b56e9e
Houda <houda.riyad-etu@etu.univh2c.ma>
2026-09-25 23:10:45 +0000

    Configuration de Spring Data REST et implémentation de projections

--- src/main/java/org/sid/bank_account_service/web/AccountRestController.java
+++ src/main/java/org/sid/bank_account_service/web/AccountRestController.java
@@ -2,17 +2,17 @@ package org.sid.bank_account_service.web;
 
 import org.sid.bank_account_service.entities.BankAccount;
 import org.sid.bank_account_service.repositories.BankAccountRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/api")
 public class AccountRestController {
     private BankAccountRepository bankAccountRepository;
-
+    //c est a dire on ne peut acceder a ce webservice que via l url /api/bankAccounts par exemple
     //injection des dependances via contructeur avec parametres
     public AccountRestController(BankAccountRepository bankAccountRepository) {
         this.bankAccountRepository = bankAccountRepository;
@@ -24,9 +24,33 @@ public class AccountRestController {
     }
 
     @GetMapping("/bankAccounts/{id}")
-    public BankAccount getBankAccount(@PathVariable String id){
+    public BankAccount bankAccount(@PathVariable String id){
         return bankAccountRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException(String.format("Account %s not found", id)));
     }
+    @PostMapping("/bankAccounts")
+    public BankAccount save(@RequestBody BankAccount bankAccount) {
+        if (bankAccount.getId() == null) {
+            bankAccount.setId(UUID.randomUUID().toString());
+        }
+        return bankAccountRepository.save(bankAccount);
+    }
 
+    @PutMapping("/bankAccounts/{id}")
+    public BankAccount update( @PathVariable String id, @RequestBody BankAccount bankAccount) {
+        BankAccount account = bankAccountRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException(String.format("Account %s not found", id)));
+        //si l utilisateur n a pas envoyer de valeur pour balance ,on aura une valeur par defaut null donc on ne mettra pas a jour la balance
+        if (bankAccount.getBalance()!=null) account.setBalance(bankAccount.getBalance());
+        if (bankAccount.getCreatedAt()!=null) account.setCreatedAt(new Date());
+        if (bankAccount.getType()!=null) account.setType(bankAccount.getType());
+        if (bankAccount.getCurrency()!=null) account.setCurrency(bankAccount.getCurrency());
+        //on ne mettera a jour [dans la base de donnees]que les champs qu' on a recu dans la requete
+        return bankAccountRepository.save(account);
+    }
+
+    @DeleteMapping("/bankAccounts/{id}")
+    public void deleteAccount(@PathVariable String id){
+        bankAccountRepository.deleteById(id);
+    }
 }
