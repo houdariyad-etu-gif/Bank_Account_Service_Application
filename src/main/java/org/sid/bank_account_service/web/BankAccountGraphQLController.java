dcb99327504dd378cabd7e06923f3d6d54e45d94
Houda <houda.riyad-etu@etu.univh2c.ma>
2026-09-26 14:05:28 +0000

    Début de l'Activité 1 <part2>: Configuration Initiale et Implémentation du Connecteur GraphQL, Schema GraphQL et du Contrôleur dans le Microservice Existant

---
+++ src/main/java/org/sid/bank_account_service/web/BankAccountGraphQLController.java
@@ -0,0 +1,21 @@
+package org.sid.bank_account_service.web;
+
+import org.sid.bank_account_service.entities.BankAccount;
+import org.sid.bank_account_service.repositories.BankAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class BankAccountGraphQLController {
+    @Autowired
+    private BankAccountRepository bankAccountRepository;
+    //annotation @QueryMapping <- pour dire que c'est une methode qui va traiter une requette GraphQL
+    @QueryMapping
+    //quand on a une requette ou le client demande accountsList on va executer la methode accountsList
+    public List<BankAccount> accountsList() {
+        return bankAccountRepository.findAll();
+    }
+}
