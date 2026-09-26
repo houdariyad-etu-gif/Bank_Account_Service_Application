af31b1ed90d9d61f92793f7f9332e625d7d7de4a
Houda <houda.riyad-etu@etu.univh2c.ma>
2026-09-26 16:06:20 +0000

    Validation de schéma et projection de requêtes GraphQL

--- src/main/java/org/sid/bank_account_service/web/BankAccountGraphQLController.java
+++ src/main/java/org/sid/bank_account_service/web/BankAccountGraphQLController.java
@@ -3,6 +3,7 @@ package org.sid.bank_account_service.web;
 import org.sid.bank_account_service.entities.BankAccount;
 import org.sid.bank_account_service.repositories.BankAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -17,5 +18,10 @@ public class BankAccountGraphQLController {
     //quand on a une requette ou le client demande accountsList on va executer la methode accountsList
     public List<BankAccount> accountsList() {
         return bankAccountRepository.findAll();
+    }
+    @QueryMapping
+    public BankAccount bankAccountById(@Argument String id) {
+        return bankAccountRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException(String.format("Account %s not found", id)));
     }
 }
